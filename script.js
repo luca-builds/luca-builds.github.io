cf0904f6eafa56bf32dea49fbe2178e86872f870
Luca Secci <lucaa.secci@gmail.com>
2026-08-24 14:36:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const modal = document.queryselector(".modal")
+const modal = document.querySelector(".modal");
+const linkInizia = document.querySelector(".inizia");
+function provaClick() {
+  console.log("hai cliccato il link, GG per te") 
+}
+
+linkInizia.addEventListener("click", provaClick)
