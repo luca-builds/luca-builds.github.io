a404822e531b3d57fa15376c4b73bd78c5d8c314
Luca Secci <lucaa.secci@gmail.com>
2026-08-25 15:58:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const modal = document.querySelector(".modal");
 const linkInizia = document.querySelector(".inizia");
+const tastoChiudi = document.querySelector(".chiudi");
 function provaClick() {
   modal.style.display = "block"
 }
-
-linkInizia.addEventListener("click", provaClick)
+function chiudiModal() {
+  modal.style.display = "none"
+}
+linkInizia.addEventListener("click", provaClick);
+tastoChiudi.addEventListener("click", chiudiModal);
